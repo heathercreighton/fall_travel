e5a8f53bd0b66445f151d728b9e5c8f2dd38ed9b
Heather Creighton <heather.creighton@gmail.com>
2016-11-30 12:03:53 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = "Michigan"
+  	@countries = ["Oman","Croatia","Peru"]
+
+  	@images = ["travel1.jpg", "travel2.jpg", "travel3.jpg", "travel4.jpg"]
+
+
+  end
+
+  def about
+   @color = params[:color]
+   @size = params[:size]
+   @pic = params[:pic]
+	end
+
+end
